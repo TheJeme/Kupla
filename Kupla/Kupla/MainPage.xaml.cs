1fd3c2a2af5ec698b1a69f0bd56101b8d9bd7732
Jeme <eemelijoonatan@gmail.com>
2020-11-04 23:45:11 +0200

    basic functionality

--- Kupla/Kupla/MainPage.xaml.cs
+++ Kupla/Kupla/MainPage.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography;
 using Xamarin.Forms;
+
+using Plugin.Clipboard;
 
 namespace Kupla
 {
@@ -16,7 +18,64 @@ namespace Kupla
 
         private void CopySecretPassword_Tapped(object sender, EventArgs e)
         {
+            CrossClipboard.Current.SetText(SecretPasswordLabel.Text);            
+        }
 
+        private void MasterPasswordEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ApplyHash();
+        }
+
+        private void ServiceEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ApplyHash();
+        }
+
+        private void ApplyHash()
+        {
+            if (MasterPasswordEntry.Text != null && ServiceEntry.Text != null)
+            {
+                SecretPasswordLabel.Text = ComputeMD5Hash(MasterPasswordEntry.Text + ServiceEntry.Text.ToUpper() + "kupla");
+            }
+            else
+            {
+                SecretPasswordLabel.Text = "";
+            }
+        }
+
+
+        static string ComputeSha256Hash(string rawData)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        static string ComputeMD5Hash(string rawdata)
+        {
+            using (MD5 md5Hash = MD5.Create())
+            {
+                byte[] bytes = md5Hash.ComputeHash(Encoding.ASCII.GetBytes(rawdata));
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < Convert.ToInt32(bytes.Length / 2); i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                for (int i = Convert.ToInt32(bytes.Length / 2); i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2").ToUpper());
+                }
+                builder.Append("!");
+                return builder.ToString();
+            }
         }
     }
 }
